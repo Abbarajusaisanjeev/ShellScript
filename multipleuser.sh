c1838e0ed9f931e68875ad975d405be0c7d6f3de
Abbarajusaisanjeev <sanjuu0039@gmail.com>
2022-03-30 09:40:55 +0530

    pushing new file

---
+++ multipleuser.sh
@@ -0,0 +1,20 @@
+#define a array Define a list having multiple entries
+userlist=`cat user.txt`
+echo $userlist
+for i in $userlist
+do
+id $i 1>/dev/null 2>/dev/null
+if [$? == 0]
+then
+    echo "user exist"
+else
+    echo "user doesnot exist can be created"
+    useradd $i
+    if [ $? == 0 ]
+    then
+        echo "user created successfully"
+    else
+        echo "error in creating user"
+    fi
+fi
+done
