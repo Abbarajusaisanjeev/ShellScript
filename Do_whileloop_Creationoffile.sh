578c075f71f84d3dfd494c293e94d9012575240e
Abbarajusaisanjeev <sanjuu0039@gmail.com>
2022-04-06 09:47:39 +0530

    this is my second commit2

--- Do_whileloop_Creationoffile.sh
+++ Do_whileloop_Creationoffile.sh
@@ -2,21 +2,21 @@
 # for i in $list 
 # do 
 # echo $i
-# test -f $i
-# if [ $? == 0 ]
-# then 
-#     echo "file already exists"
-# else 
-#     echo "File doesnot exists can be created"
-#     touch $i
-#     if [ $? == 0 ]
-#     then 
-#         echo "file created sucessfully"
-#     else 
-#         echo "Error in file creation"
-#     fi 
-# fi
-# done 
+test -f $i
+if [ $? == 0 ]
+then 
+    echo "file already exists"
+else 
+    echo "File doesnot exists can be created"
+    touch $i
+    if [ $? == 0 ]
+    then 
+        echo "file created sucessfully"
+    else 
+        echo "Error in file creation"
+    fi 
+fi
+done 
 list="file1 file2 file3"
 for i in $list
 do 
